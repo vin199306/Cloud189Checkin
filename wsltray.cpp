99381e06a10449e9939a87236e632b5f7ad80435
Yanwen <66867243+vin199306@users.noreply.github.com>
2025-07-18 22:45:38 +0800

    4

--- wsltray.cpp
+++ wsltray.cpp
@@ -6,7 +6,6 @@
 #include <thread>
 #include <atomic>
 #include <chrono>
-#include <wchar.h>
 
 // 全局变量
 NOTIFYICONDATA nid;
@@ -64,13 +63,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
     nid.uCallbackMessage = WM_USER + 1;
     nid.hIcon = hIconRed;
-    wcscpy_s(nid.szTip, "WSL 未运行");
+    wcscpy_s(nid.szTip,100, L"WSL 未运行");
     
     Shell_NotifyIcon(NIM_ADD, &nid);
     
     // 创建右键菜单
     hMenu = CreatePopupMenu();
-    AppendMenu(hMenu, MF_STRING, 1, "退出");
+    AppendMenu(hMenu, MF_STRING, 1, L"退出");
     
     // 启动WSL监控线程
     std::thread monitorThread(MonitorWslStatus);
@@ -177,7 +176,7 @@ void ToggleWsl() {
 
 void UpdateIcon() {
     nid.hIcon = wslRunning ? hIconGreen : hIconRed;
-    wcscpy_s(nid.szTip, wslRunning ? "WSL 正在运行" : "WSL 未运行");
+    wcscpy_s(nid.szTip, 100,wslRunning ? L"WSL 正在运行" : L"WSL 未运行");
     Shell_NotifyIcon(NIM_MODIFY, &nid);
 }
 
@@ -230,4 +229,4 @@ HICON CreateColoredIcon(COLORREF color) {
     DeleteObject(hbmMask);
     
     return hIcon;
-}        
+}
